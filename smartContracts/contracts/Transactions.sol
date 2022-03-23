ea58687c4a246caf92f8dc9543273af4785f2c06
Chaudhary Shaharyar Tariq <shaharyar.tariq248@gmail.com>
2022-03-24 00:39:51 +0500

    transaction error remains unsolved

--- smartContracts/contracts/Transactions.sol
+++ smartContracts/contracts/Transactions.sol
@@ -1,50 +1,54 @@
 // SPDX-License-Identifier: UNLICENSED
+
 pragma solidity ^0.8.0;
 
 contract Transactions {
-    uint256 transactionsCounter;
+    uint256 transactionCount;
 
     event Transfer(
         address from,
-        address reciever,
+        address receiver,
         uint256 amount,
-        uint256 timeStamp,
         string message,
+        uint256 timestamp,
         string keyword
     );
+
     struct TransferStruct {
         address sender;
-        address reciever;
+        address receiver;
         uint256 amount;
-        uint256 timeStamp;
         string message;
+        uint256 timestamp;
         string keyword;
     }
+
     TransferStruct[] transactions;
 
     function addToBlockchain(
-        address payable reciever,
+        address payable receiver,
         uint256 amount,
         string memory message,
         string memory keyword
     ) public {
-        transactionsCounter++;
+        transactionCount += 1;
         transactions.push(
             TransferStruct(
                 msg.sender,
-                reciever,
+                receiver,
                 amount,
-                block.timestamp,
                 message,
+                block.timestamp,
                 keyword
             )
         );
+
         emit Transfer(
             msg.sender,
-            reciever,
+            receiver,
             amount,
-            block.timestamp,
             message,
+            block.timestamp,
             keyword
         );
     }
@@ -58,6 +62,6 @@ contract Transactions {
     }
 
     function getTransactionCount() public view returns (uint256) {
-        return transactionsCounter;
+        return transactionCount;
     }
 }
--- smartContracts/hardhat.config.js
+++ smartContracts/hardhat.config.js
@@ -4,9 +4,9 @@ module.exports = {
   solidity: "0.8.0",
   networks: {
     ropsten: {
-      url: "https://eth-ropsten.alchemyapi.io/v2/xzSfy9phyvjuPdLL6NI2FAzbsPYIe7Y9",
+      url: "https://eth-ropsten.alchemyapi.io/v2/-FhNfMnnxYNjzQqJMODvEULXJ6zOfQcI",
       accounts: [
-        "46f31d7f22f8b36dbbbfcf2520c229053671fdca03dcd65e8f2e8b8e571b4050",
+        "169f5e988e9e80927a55ad358ef2d3a91fb973132ea5bfb052ff1413e6473c48",
       ],
     },
   },
