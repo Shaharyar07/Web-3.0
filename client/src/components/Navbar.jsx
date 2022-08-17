541769b19c0077fd9de3067acd2704959b59e4c9
Shaharyar07 <shaharyar.tariq248@gmail.com>
2022-08-18 01:07:32 +0500

    deployed

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -12,48 +12,48 @@ const Navbar = () => {
   const [toggleMenu, setToggleMenu] = React.useState(false);
 
   return (
-    <nav className="w-full flex md:justify-center justify-between items-center p-4">
-      <div className="md:flex-[0.5] flex-initial justify-center items-center">
-        <Link to="/">
-          <img src={logo} alt="logo" className="w-32 cursor-pointer" />
+    <nav className='w-full flex md:justify-center justify-between items-center p-4'>
+      <div className='md:flex-[0.5] flex-initial justify-center items-center'>
+        <Link to='/'>
+          <img src={logo} alt='logo' className='w-32 cursor-pointer' />
         </Link>
-      </div>  
-      <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
+      </div>
+      <ul className='text-white md:flex hidden list-none flex-row justify-between items-center flex-initial'>
         {["Market", "Exchange"].map((item, index) => (
           <NavBarItem key={item + index} title={item} />
         ))}
-        <Link to="/transactions">
-          <NavBarItem title="Transactions" />
+        <Link to='/transactions'>
+          <NavBarItem title='Transactions' />
         </Link>
       </ul>
-      <div className="flex relative">
+      <div className='flex relative'>
         {!toggleMenu && (
           <HiMenuAlt4
             fontSize={28}
-            className="text-white md:hidden cursor-pointer"
+            className='text-white md:hidden cursor-pointer'
             onClick={() => setToggleMenu(true)}
           />
         )}
         {toggleMenu && (
           <AiOutlineClose
             fontSize={28}
-            className="text-white md:hidden cursor-pointer"
+            className='text-white md:hidden cursor-pointer'
             onClick={() => setToggleMenu(false)}
           />
         )}
         {toggleMenu && (
           <ul
-            className="z-10 fixed -top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
-            flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in"
+            className='z-10 fixed -top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
+            flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in'
           >
-            <li className="text-xl w-full my-2">
+            <li className='text-xl w-full my-2'>
               <AiOutlineClose onClick={() => setToggleMenu(false)} />
             </li>
-            {["Market", "Exchange", "Transactions"].map((item, index) => (
+            {["Transactions", "Market", "Exchange"].map((item, index) => (
               <NavBarItem
                 key={item + index}
                 title={item}
-                classprops="my-2 text-lg"
+                classprops='my-2 text-lg'
               />
             ))}
           </ul>
